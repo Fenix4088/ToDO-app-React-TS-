f3df7383b8b32f1bc3afd7103c501fa13a42a374
Yehor Pliasov <fenix4088@gmail.com>
2021-03-17 10:33:41 +0100

    Updates

---
+++ precache-manifest.932237a2cf012f0911ed772339dd34b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8692937544cb9e91ae0f1ea46aa9e02f",
+    "url": "/ToDO-app-React-TS-/index.html"
+  },
+  {
+    "revision": "a7fee653b98d8f70f932",
+    "url": "/ToDO-app-React-TS-/static/css/main.c883d321.chunk.css"
+  },
+  {
+    "revision": "ebe337a577340d41e930",
+    "url": "/ToDO-app-React-TS-/static/js/2.803782b7.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/ToDO-app-React-TS-/static/js/2.803782b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7fee653b98d8f70f932",
+    "url": "/ToDO-app-React-TS-/static/js/main.c6af2f89.chunk.js"
+  },
+  {
+    "revision": "19254da3f82ee9908930",
+    "url": "/ToDO-app-React-TS-/static/js/runtime-main.edc203a8.js"
+  }
+]);
